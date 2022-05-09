310e6c04608016d2f9066406f6929f79944af7cb
Roy Okello <therealokello@outlook.com>
2022-05-09 14:36:30 +0300

    Update nova.rs

--- src/state/nova.rs
+++ src/state/nova.rs
@@ -1,7 +1,6 @@
-use crate::STELAR_ADDRESS;
 use astro_format::string;
 use neutrondb::Store;
-use opis::Int;
+use opis::{Int, modulo};
 
 use crate::{blocks::Block, NOVA_ADDRESS, accounts::Account};
 
@@ -24,7 +23,39 @@ pub fn validator_selection(accounts_store: &Store, latest_block: &Block, target_
     } else {
         Int::zero()
     };
+
+    let mut random = Int::from_bytes(&latest_block.hash());
+
+    if misses > Int::zero() {
+        
+        let shifts = u64::from_be_bytes(misses.to_ext_bytes(8).try_into().unwrap());
+
+        for _ in 0..shifts {
+            random = random.lfsr();
+        }
+            
+    };
+
+    let sum = nova_account.storage.iter().fold(Int::zero(), |acc, x| acc + Int::from_bytes(x.1));
     
-    STELAR_ADDRESS
+    random = modulo(&random, &sum);
+
+    let mut validator = [0_u8;32];
+
+    for x in nova_account.storage {
+
+        random = random - Int::from_bytes(&x.1);
+
+        if random <= Int::zero() {
+
+            validator = x.0;
+            
+            break;
+        
+        }
+
+    }
+
+    validator
 
 }
