aa28532e9abdd52d535d8dafb85b3bde63793c3a
Roy Okello <royokello@protonmail.com>
2023-04-06 12:43:23 +0300

    Update topic.rs
    - changed Block -> State
    - changed BlockRequest -> StateRequest
    - added Object and ObjectRequest

--- src/topic.rs
+++ src/topic.rs
@@ -2,11 +2,13 @@ use std::error::Error;
 
 #[derive(Clone, Debug)]
 pub enum Topic {
+    Object,
+    ObjectRequest,
     Ping,
-    RouteRequest,
     Route,
-    BlockRequest,
-    Block,
+    RouteRequest,
+    State,
+    StateRequest,
     Transaction,
 }
 
@@ -15,12 +17,14 @@ impl Into<Vec<u8>> for Topic {
     fn into(self) -> Vec<u8> {
 
         match self {
-            Topic::Ping => vec![1],
-            Topic::RouteRequest => vec![2],
+            Topic::Object => vec![0],
+            Topic::ObjectRequest => vec![1],
+            Topic::Ping => vec![2],
             Topic::Route => vec![3],
-            Topic::BlockRequest => vec![4],
-            Topic::Block => vec![5],
-            Topic::Transaction => vec![6],
+            Topic::RouteRequest => vec![4],
+            Topic::State => vec![5],
+            Topic::StateRequest => vec![6],
+            Topic::Transaction => vec![7],
         }
 
     }
@@ -35,12 +39,14 @@ impl TryFrom<&[u8]> for Topic {
     fn try_from(value: &[u8]) -> Result<Self, Box<dyn Error>> {
 
         match value {
-            [1] => Ok(Topic::Ping),
-            [2] => Ok(Topic::RouteRequest),
+            [0] => Ok(Topic::Object),
+            [1] => Ok(Topic::ObjectRequest),
+            [2] => Ok(Topic::Ping),
             [3] => Ok(Topic::Route),
-            [4] => Ok(Topic::BlockRequest),
-            [5] => Ok(Topic::Block),
-            [6] => Ok(Topic::Transaction),
+            [4] => Ok(Topic::RouteRequest),
+            [5] => Ok(Topic::State),
+            [6] => Ok(Topic::StateRequest),
+            [7] => Ok(Topic::Transaction),
             _ => Err("topic decoding error!")?
         }
 
